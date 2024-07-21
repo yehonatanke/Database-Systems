055e9f8bd29a2916c729e3131c6cfc06c4549515
yehonatanke <150778110+yehonatanke@users.noreply.github.com>
2024-07-21 22:44:15 +0300

    Update university_scheduler_schema.sql

--- Chapter 02/PostgreSQL/UniScheduler/university_scheduler_schema.sql
+++ Chapter 02/PostgreSQL/UniScheduler/university_scheduler_schema.sql
@@ -7,14 +7,14 @@ DROP TABLE IF EXISTS schedule CASCADE;
 -- Room table
 -- Stores information about university rooms
 CREATE TABLE room (
-    rno INTEGER,              -- Room number
-    bno INTEGER,              -- Building number
+    rno INT,              -- Room number
+    bno INT,              -- Building number
     function VARCHAR(50) NOT NULL,  -- Room function (e.g., lecture hall, lab)
     capacity INTEGER NOT NULL,  -- Room capacity (number of people)
     PRIMARY KEY (rno, bno),   -- Composite primary key
-    CHECK (rno > 0),
-    CHECK (bno > 0),
-    CHECK (capacity > 0)
+    CHECK (rno >= 0),
+    CHECK (bno >= 0),
+    CHECK (capacity >= 0)
 );
 
 -- Semester table
@@ -30,28 +30,26 @@ CREATE TABLE semester (
 -- Stores information about regularly scheduled events (e.g., weekly classes)
 CREATE TABLE revent (
     reid INTEGER PRIMARY KEY,  -- Regular event ID
-    cno VARCHAR(10) NOT NULL,  -- Course number
-    sid CHAR NOT NULL,         -- Semester ID (foreign key)
-    dayofweek INTEGER NOT NULL,  -- Day of the week (1-7)
+    cno INTEGER,  -- Course number
+    sid CHAR(10),           -- Semester ID (foreign key)
+    dayofweek VARCHAR(10),  -- Day of the week (1-7)
     stime TIME NOT NULL,       -- Start time of the event
-    duration INTEGER NOT NULL, -- Duration in minutes
+    duration INTERVAL,         -- Duration in minutes
     reg INTEGER NOT NULL,      -- Number of registrants
-    FOREIGN KEY (sid) REFERENCES semester(sid),
-    CHECK (dayofweek BETWEEN 1 AND 7),
-    CHECK (duration > 0),
+    FOREIGN KEY (sid) REFERENCES semester,
     CHECK (reg >= 0)
 );
 
 -- Sevent (Special Event) table
 -- Stores information about one-time or special events
 CREATE TABLE sevent (
-    seid INTEGER PRIMARY KEY,  -- Special event ID
+    seid INTEGER,              -- Special event ID
     ename VARCHAR(100) NOT NULL,  -- Event name
     edate DATE NOT NULL,       -- Event date
     stime TIME NOT NULL,       -- Start time of the event
-    duration INTEGER NOT NULL, -- Duration in minutes
-    reg INTEGER NOT NULL,      -- Number of registrants
-    CHECK (duration > 0),
+    duration INTERVAL, 		   -- Duration in minutes
+    reg INT,      -- Number of registrants
+  	PRIMARY KEY (seid),
     CHECK (reg >= 0)
 );
 
@@ -60,12 +58,11 @@ CREATE TABLE sevent (
 CREATE TABLE schedule (
     sdate DATE,       -- Date of the scheduled event
     stime TIME,       -- Start time of the scheduled event
-    rno INTEGER,      -- Room number (part of foreign key to room)
-    bno INTEGER,      -- Building number (part of foreign key to room)
-    eid INTEGER,      -- Event ID (could be reid or seid)
-    duration INTEGER, -- Duration of the scheduled event
+    rno INT,      -- Room number (part of foreign key to room)
+    bno INT,      -- Building number (part of foreign key to room)
+    eid INT,      -- Event ID (could be reid or seid)
+    duration INTERVAL, -- Duration of the scheduled event
     PRIMARY KEY (sdate, stime, rno, bno, eid),  -- Updated primary key including eid
-    FOREIGN KEY (rno, bno) REFERENCES room(rno, bno),
-    CHECK (duration > 0)
+    FOREIGN KEY (rno, bno) REFERENCES room
 );
 
